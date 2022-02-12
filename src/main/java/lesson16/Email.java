40c9454c2271631b5733f82890ae8b132aff2e05
Andrei <surginevich1998@gmail.com>
2022-02-12 21:44:04 +0300

    delete class EmailTest

--- src/main/java/lesson16/Email.java
+++ src/main/java/lesson16/Email.java
@@ -1,22 +1,19 @@
 package lesson16;
 
-import static lesson16.Email.checkEmail;
-
 public class Email {
     static void checkEmail(String emailList) {
         int c = 0;
-        for (int i = 0; c < emailList.length()-1; i++) {
+        for (int i = 0; c < emailList.length() - 1; i++) {
             int a = emailList.indexOf('@', c);
             int b = emailList.indexOf('.', c);
             c = emailList.indexOf(';', c + 1);
             System.out.println(emailList.substring(a + 1, b));
         }
     }
-}
 
-class EmailTest {
     public static void main(String[] args) {
         checkEmail("ya@yahoo.com; on@mail.ru; ona@gmail.com;");
     }
 }
+
 
